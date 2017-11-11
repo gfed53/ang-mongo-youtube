fd37734712454cb4e9d1e09f3fc18b8b79a5418e
gfed53 <gfederico7153@gmail.com>
2017-11-11 14:55:51 -0500

    Updated app to use scss, imported simple-grid to be used, adjusted style encapsulation for components

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { ViewEncapsulation } from '@angular/core';
 
 import { SearchService } from '../services/search.service';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.css']
+  styleUrls: ['./search.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class SearchComponent implements OnInit {
 
